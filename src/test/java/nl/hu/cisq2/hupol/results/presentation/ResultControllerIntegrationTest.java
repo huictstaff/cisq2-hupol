512ef07f5f58725485dcf75b7222f55123388274
Hugo-Helder <59727184+Hugo-Helder@users.noreply.github.com>
2025-02-13 11:02:33 +0100

    Update for deprecated mockbean

--- src/test/java/nl/hu/cisq2/hupol/results/presentation/ResultControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq2/hupol/results/presentation/ResultControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class ResultControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ResultService service;
 
     @Test
