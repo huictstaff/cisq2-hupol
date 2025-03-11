2bc3ac0206a1dcba88b0f4a1afedfa0e5f478ed8
HUGO <hugo.helder@hu.nl>
2025-03-11 11:18:40 +0100

    Remove unused import
    Signed-off-by: HUGO <hugo.helder@hu.nl>

--- src/test/java/nl/hu/cisq2/hupol/results/presentation/ResultControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq2/hupol/results/presentation/ResultControllerIntegrationTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
